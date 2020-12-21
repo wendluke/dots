93567a9f4d80a024c258667bc61bfab0ecc46eee
Lucas Wend <wendluke@gmail.com>
2020-12-20 22:24:41 -0300

    stuff

---
+++ .config/awesome/themes/vertex/theme.lua
@@ -0,0 +1,305 @@
+--[[
+
+     Vertex Awesome WM theme
+     github.com/lcpz
+
+--]]
+
+local gears = require("gears")
+local lain  = require("lain")
+local awful = require("awful")
+local wibox = require("wibox")
+local dpi   = require("beautiful.xresources").apply_dpi
+
+local math, string, tag, tonumber, type, os = math, string, tag, tonumber, type, os
+local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
+
+local theme                                     = {}
+theme.default_dir                               = require("awful.util").get_themes_dir() .. "default"
+theme.icon_dir                                  = os.getenv("HOME") .. "/.config/awesome/themes/vertex/icons"
+theme.font                                      = "Roboto Bold 10"
+theme.taglist_font                              = "FontAwesome 17"
+theme.fg_normal                                 = "#cfb79f"
+theme.fg_focus                                  = "#cfb79f"
+theme.bg_focus                                  = "#141c21"
+theme.bg_focus2                                 = "#cfb79f"
+theme.bg_normal                                 = "#141c21"
+theme.fg_urgent                                 = "#CC9393"
+theme.bg_urgent                                 = "#006B8E"
+theme.border_width                              = dpi(1)
+theme.border_normal                             = "#263640"
+theme.border_focus                              = "#cfb79f"
+theme.tooltip_border_color                      = theme.fg_focus
+theme.tooltip_border_width                      = theme.border_width
+theme.menu_height                               = dpi(24)
+theme.menu_width                                = dpi(140)
+theme.awesome_icon                              = theme.icon_dir .. "/awesome.png"
+theme.taglist_squares_sel                       = gears.surface.load_from_shape(dpi(3), dpi(30), gears.shape.rectangle, theme.fg_focus)
+theme.taglist_squares_unsel                     = gears.surface.load_from_shape(dpi(3), dpi(30), gears.shape.rectangle, theme.bg_focus2)
+theme.panelbg                                   = theme.icon_dir .. "/panel.png"
+theme.bat000charging                            = theme.icon_dir .. "/bat-000-charging.png"
+theme.bat000                                    = theme.icon_dir .. "/bat-000.png"
+theme.bat020charging                            = theme.icon_dir .. "/bat-020-charging.png"
+theme.bat020                                    = theme.icon_dir .. "/bat-020.png"
+theme.bat040charging                            = theme.icon_dir .. "/bat-040-charging.png"
+theme.bat040                                    = theme.icon_dir .. "/bat-040.png"
+theme.bat060charging                            = theme.icon_dir .. "/bat-060-charging.png"
+theme.bat060                                    = theme.icon_dir .. "/bat-060.png"
+theme.bat080charging                            = theme.icon_dir .. "/bat-080-charging.png"
+theme.bat080                                    = theme.icon_dir .. "/bat-080.png"
+theme.bat100charging                            = theme.icon_dir .. "/bat-100-charging.png"
+theme.bat100                                    = theme.icon_dir .. "/bat-100.png"
+theme.batcharged                                = theme.icon_dir .. "/bat-charged.png"
+theme.ethon                                     = theme.icon_dir .. "/ethernet-connected.png"
+theme.ethoff                                    = theme.icon_dir .. "/ethernet-disconnected.png"
+theme.volhigh                                   = theme.icon_dir .. "/volume-high.png"
+theme.vollow                                    = theme.icon_dir .. "/volume-low.png"
+theme.volmed                                    = theme.icon_dir .. "/volume-medium.png"
+theme.volmutedblocked                           = theme.icon_dir .. "/volume-muted-blocked.png"
+theme.volmuted                                  = theme.icon_dir .. "/volume-muted.png"
+theme.voloff                                    = theme.icon_dir .. "/volume-off.png"
+theme.wifidisc                                  = theme.icon_dir .. "/wireless-disconnected.png"
+theme.wififull                                  = theme.icon_dir .. "/wireless-full.png"
+theme.wifihigh                                  = theme.icon_dir .. "/wireless-high.png"
+theme.wifilow                                   = theme.icon_dir .. "/wireless-low.png"
+theme.wifimed                                   = theme.icon_dir .. "/wireless-medium.png"
+theme.wifinone                                  = theme.icon_dir .. "/wireless-none.png"
+theme.layout_fairh                              = theme.default_dir.."/layouts/fairhw.png"
+theme.layout_fairv                              = theme.default_dir.."/layouts/fairvw.png"
+theme.layout_floating                           = theme.default_dir.."/layouts/floatingw.png"
+theme.layout_magnifier                          = theme.default_dir.."/layouts/magnifierw.png"
+theme.layout_max                                = theme.default_dir.."/layouts/maxw.png"
+theme.layout_fullscreen                         = theme.default_dir.."/layouts/fullscreenw.png"
+theme.layout_tilebottom                         = theme.default_dir.."/layouts/tilebottomw.png"
+theme.layout_tileleft                           = theme.default_dir.."/layouts/tileleftw.png"
+theme.layout_tile                               = theme.default_dir.."/layouts/tilew.png"
+theme.layout_tiletop                            = theme.default_dir.."/layouts/tiletopw.png"
+theme.layout_spiral                             = theme.default_dir.."/layouts/spiralw.png"
+theme.layout_dwindle                            = theme.default_dir.."/layouts/dwindlew.png"
+theme.layout_cornernw                           = theme.default_dir.."/layouts/cornernww.png"
+theme.layout_cornerne                           = theme.default_dir.."/layouts/cornernew.png"
+theme.layout_cornersw                           = theme.default_dir.."/layouts/cornersww.png"
+theme.layout_cornerse                           = theme.default_dir.."/layouts/cornersew.png"
+theme.layout_centerwork                         = theme.default_dir.."/layouts/centerworkw.png"
+theme.tasklist_plain_task_name                  = true
+theme.tasklist_disable_icon                     = true
+theme.useless_gap                               = dpi(5)
+theme.titlebar_close_button_normal              = theme.default_dir.."/titlebar/close_normal.png"
+theme.titlebar_close_button_focus               = theme.default_dir.."/titlebar/close_focus.png"
+theme.titlebar_minimize_button_normal           = theme.default_dir.."/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus            = theme.default_dir.."/titlebar/minimize_focus.png"
+theme.titlebar_ontop_button_normal_inactive     = theme.default_dir.."/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive      = theme.default_dir.."/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active       = theme.default_dir.."/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active        = theme.default_dir.."/titlebar/ontop_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive    = theme.default_dir.."/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive     = theme.default_dir.."/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active      = theme.default_dir.."/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active       = theme.default_dir.."/titlebar/sticky_focus_active.png"
+theme.titlebar_floating_button_normal_inactive  = theme.default_dir.."/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive   = theme.default_dir.."/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active    = theme.default_dir.."/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active     = theme.default_dir.."/titlebar/floating_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = theme.default_dir.."/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.default_dir.."/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active   = theme.default_dir.."/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active    = theme.default_dir.."/titlebar/maximized_focus_active.png"
+
+-- http://fontawesome.io/cheatsheet
+awful.util.tagnames = { "", "", "", "" }
+
+local markup = lain.util.markup
+
+-- Clock
+--os.setlocale(os.getenv("LANG")) -- to localize the clock
+local mytextclock = wibox.widget.textclock(markup("#CFB79F", "%a %d %b, %H:%M"))
+mytextclock.font = theme.font
+theme.cal = lain.widget.cal({
+    attach_to = { mytextclock },
+    notification_preset = {
+        fg = "#FFFFFF",
+        bg = theme.bg_normal,
+        position = "top_middle",
+        font = "Monospace 10"
+    }
+})
+
+
+-- MPD
+theme.mpd = lain.widget.mpd({
+    music_dir = "/mnt/storage/Downloads/Music",
+    settings = function()
+        if mpd_now.state == "play" then
+            title = mpd_now.title
+            artist  = "  " .. mpd_now.artist  .. " "
+        elseif mpd_now.state == "pause" then
+            title = "mpd "
+            artist  = "paused "
+        else
+            title  = ""
+            artist = ""
+        end
+
+        widget:set_markup(markup.font(theme.font, title .. markup(theme.fg_focus, artist)))
+    end
+})
+
+-- ALSA volume
+local volicon = wibox.widget.imagebox(theme.widget_vol)
+theme.volume = lain.widget.alsa({
+    settings = function()
+        if volume_now.status == "off" then
+            volicon:set_image(theme.widget_vol_mute)
+        elseif tonumber(volume_now.level) == 0 then
+            volicon:set_image(theme.widget_vol_no)
+        elseif tonumber(volume_now.level) <= 50 then
+            volicon:set_image(theme.widget_vol_low)
+        else
+            volicon:set_image(theme.widget_vol)
+        end
+
+        widget:set_markup(markup.font(theme.font, " " .. volume_now.level .. "% "))
+    end
+})
+theme.volume.widget:buttons(awful.util.table.join(
+                               awful.button({}, 4, function ()
+                                     awful.util.spawn("amixer set Master 1%+")
+                                     theme.volume.update()
+                               end),
+                               awful.button({}, 5, function ()
+                                     awful.util.spawn("amixer set Master 1%-")
+                                     theme.volume.update()
+                               end)
+))
+
+volicon:buttons(my_table.join (
+          awful.button({}, 1, function()
+            awful.spawn(string.format("%s -e alsamixer", awful.util.terminal))
+          end),
+          awful.button({}, 2, function()
+            os.execute(string.format("%s set %s 100%%", theme.volume.cmd, theme.volume.channel))
+            theme.volume.notify()
+          end),
+          awful.button({}, 3, function()
+            os.execute(string.format("%s set %s toggle", theme.volume.cmd, theme.volume.togglechannel or theme.volume.channel))
+            theme.volume.notify()
+          end),
+          awful.button({}, 4, function()
+            os.execute(string.format("%s set %s 1%%+", theme.volume.cmd, theme.volume.channel))
+            theme.volume.notify()
+          end),
+          awful.button({}, 5, function()
+            os.execute(string.format("%s set %s 1%%-", theme.volume.cmd, theme.volume.channel))
+            theme.volume.notify()
+          end)
+))
+
+-- Weather
+theme.weather = lain.widget.weather({
+    city_id = 6323121, -- placeholder (London)
+    notification_preset = { font = "Monospace 10" },
+    settings = function()
+        units = math.floor(weather_now["main"]["temp"])
+        widget:set_markup(" " .. markup.font(theme.font, units .. "°C") .. " ")
+    end
+})
+
+-- Separators
+local space = wibox.widget.textbox(" ")
+local rspace1 = wibox.widget.textbox()
+local rspace0 = wibox.widget.textbox()
+local rspace2 = wibox.widget.textbox()
+local rspace3 = wibox.widget.textbox()
+local tspace1 = wibox.widget.textbox()
+tspace1.forced_width = dpi(18)
+rspace1.forced_width = dpi(16)
+rspace0.forced_width = dpi(18)
+rspace2.forced_width = dpi(19)
+rspace3.forced_width = dpi(21)
+
+local lspace1 = wibox.widget.textbox()
+local lspace2 = wibox.widget.textbox()
+local lspace3 = wibox.widget.textbox()
+lspace1.forced_height = dpi(18)
+lspace2.forced_height = dpi(10)
+lspace3.forced_height = dpi(16)
+
+local barcolor = gears.color({
+    type  = "linear",
+    from  = { 0, dpi(46) },
+    to    = { dpi(46), dpi(46) },
+    stops = { {0, theme.bg_focus}, {0.9, theme.bg_focus2} }
+})
+
+local barcolor2 = gears.color({
+    type  = "linear",
+    from  = { 0, dpi(46) },
+    to    = { dpi(46), dpi(46) },
+    stops = { {0, "#323232"}, {1, theme.bg_normal} }
+})
+
+function theme.at_screen_connect(s)
+    -- Quake application
+    s.quake = lain.util.quake({ app = awful.util.terminal, border = theme.border_width })
+
+    -- Tags
+    awful.tag(awful.util.tagnames, s, awful.layout.layouts[1])
+
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    s.mypromptbox.bg = "#00000000"
+
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(my_table.join(
+                           awful.button({}, 1, function () awful.layout.inc( 1) end),
+                           awful.button({}, 2, function () awful.layout.set( awful.layout.layouts[1] ) end),
+                           awful.button({}, 3, function () awful.layout.inc(-1) end),
+                           awful.button({}, 4, function () awful.layout.inc( 1) end),
+                           awful.button({}, 5, function () awful.layout.inc(-1) end)))
+
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
+
+    -- Create a tasklist widget
+    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.focused, awful.util.tasklist_buttons, { bg_focus = "#00000000" })
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = dpi(25), bg = gears.color.create_png_pattern(theme.panelbg), opacity = 0.95 })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        expand = "none",
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            s.mypromptbox,
+            tspace1,
+            s.mytasklist,
+        },
+        { -- Middle widgets
+            layout = wibox.layout.flex.horizontal,
+            max_widget_size = 1500,
+            mytextclock
+        },
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            wibox.widget { nil, nil, theme.mpd.widget, layout = wibox.layout.align.horizontal },
+            volicon,
+            rspace0,
+            -- theme.weather.icon,
+            theme.weather.widget,
+            rspace0,
+            wibox.widget.systray(),
+            rspace0,
+            -- s.mytaglist,
+            s.mylayoutbox,
+            rspace1
+        },
+    }
+
+    -- gears.timer.delayed_call(theme.vertical_wibox, s)
+end
+
+return theme
